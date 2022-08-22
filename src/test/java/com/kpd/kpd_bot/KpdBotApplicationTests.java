27462b2f750549548362c1a42431639783345ffb
brash-ram <rudi.will.do.it@gmail.com>
2022-08-22 22:16:07 +0300

    first commit

---
+++ src/test/java/com/kpd/kpd_bot/KpdBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpd.kpd_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpdBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
