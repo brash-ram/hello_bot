d25be073e1b8296ffd0f9f8b99e729af08cef426
brash-ram <rudi.will.do.it@gmail.com>
2022-09-07 12:39:23 +0300

    add: work with keyboard and send message

--- src/main/java/com/kpd/kpd_bot/bot/MessageHandler.java
+++ src/main/java/com/kpd/kpd_bot/bot/MessageHandler.java
@@ -1,5 +1,6 @@
 package com.kpd.kpd_bot.bot;
 
+import com.kpd.kpd_bot.message_services.MainMessageConstructor;
 import com.kpd.kpd_bot.statics.Buttons;
 import com.kpd.kpd_bot.statics.StringConst;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -10,19 +11,21 @@ public class MessageHandler {
 	public static MessageAdapter handleMessage(Update update){
 		Message message = update.getMessage();
 		String messageText = message.getText();
-		MessageAdapter newMessage = new MessageAdapter();
+		MessageAdapter newMessage = new MessageAdapter().setChatId(message.getChatId());
 
 
 		switch (messageText) {
 			case "/start":
-				newMessage.setChatId(message.getChatId())
-						.setText(StringConst.startMessage)
+				newMessage.setText(StringConst.startMessage)
 						.addReplyButtons(Buttons.startButtons);
 				break;
 
+			case "Приветственное сообщение":
+				newMessage.setText(MainMessageConstructor.getMessage());
+				break;
+
 			default:
-				newMessage.setChatId(message.getChatId())
-						.setText(StringConst.hui);
+				newMessage.setText(StringConst.hui);
 				break;
 		}
 		return newMessage;
--- src/main/java/com/kpd/kpd_bot/message_services/MainMessageConstructor.java
+++ src/main/java/com/kpd/kpd_bot/message_services/MainMessageConstructor.java
@@ -1,8 +1,10 @@
 package com.kpd.kpd_bot.message_services;
 
+import com.kpd.kpd_bot.statics.StringConst;
+
 public class MainMessageConstructor {
 
-	public void sendMessageUser(String chatId) {
-
+	public static String getMessage() {
+		return StringConst.helloMessage;
 	}
 }
