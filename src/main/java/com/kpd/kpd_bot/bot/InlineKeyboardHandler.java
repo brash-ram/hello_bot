3eaa41c28034c5dd2261cfd4faec0de5f6d5414d
brash-ram <rudi.will.do.it@gmail.com>
2022-10-02 00:26:10 +0300

    add: fix set time send function

--- src/main/java/com/kpd/kpd_bot/bot/InlineKeyboardHandler.java
+++ src/main/java/com/kpd/kpd_bot/bot/InlineKeyboardHandler.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Service
@@ -18,23 +19,17 @@ public class InlineKeyboardHandler {
 		int messageId = update.getCallbackQuery().getMessage().getMessageId();
 		long chatId = update.getCallbackQuery().getMessage().getChatId();
 		String messageText = update.getCallbackQuery().getMessage().getText();
-		MessageAdapter newMessage = new MessageAdapter().setChatId(chatId);
-		EditMessageText editMessage = new EditMessageText();
+		MessageAdapter newMessage = null;
+		EditMessageText editMessage = null;
 
 		switch (callData) {
 			case "<<":
-				editMessage.setChatId(chatId);
-				editMessage.setMessageId(messageId);
-				editMessage.setText(timeSendInlineKeyboardHandler.subHour(messageText));
-				editMessage.setReplyMarkup(timeSendInlineKeyboardHandler.getKeyboard());
-				newMessage = null;
+				editMessage = this.editMessage(chatId, messageId,
+						timeSendInlineKeyboardHandler.subHour(messageText), update.getCallbackQuery().getMessage().getReplyMarkup());
 				break;
 			case ">>":
-				editMessage.setChatId(chatId);
-				editMessage.setMessageId(messageId);
-				editMessage.setText(timeSendInlineKeyboardHandler.addHour(messageText));
-				editMessage.setReplyMarkup(timeSendInlineKeyboardHandler.getKeyboard());
-				newMessage = null;
+				editMessage = this.editMessage(chatId, messageId,
+						timeSendInlineKeyboardHandler.addHour(messageText), update.getCallbackQuery().getMessage().getReplyMarkup());
 				break;
 		}
 
@@ -46,7 +41,12 @@ public class InlineKeyboardHandler {
 		}
 	}
 
-//	private EditMessageText editMessage(long chatId, int messageId, String text) {
-//
-//	}
+	private EditMessageText editMessage(long chatId, int messageId, String text, InlineKeyboardMarkup keyboardMarkup) {
+		EditMessageText editMessage = new EditMessageText();
+		editMessage.setChatId(chatId);
+		editMessage.setMessageId(messageId);
+		editMessage.setText(text);
+		editMessage.setReplyMarkup(keyboardMarkup);
+		return editMessage;
+	}
 }
