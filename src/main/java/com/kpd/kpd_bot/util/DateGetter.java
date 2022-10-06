56f7ae432c9edbb17ddfeec3918ca398af9b0ec8
brash-ram <rudi.will.do.it@gmail.com>
2022-10-06 23:31:52 +0300

    add: sender message on selected hour

--- src/main/java/com/kpd/kpd_bot/util/DateGetter.java
+++ src/main/java/com/kpd/kpd_bot/util/DateGetter.java
@@ -4,11 +4,22 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
-import java.sql.Timestamp;
+import java.time.LocalTime;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DateGetter {
+	public static final String DEFAULT_HOUR = "no date";
 	public static Date getSqlDate() {
 		return new Date(System.currentTimeMillis());
+	}
+
+	public static String getCurrentHourIfNewHour() {
+		LocalTime now = LocalTime.now();
+		String result = DateGetter.DEFAULT_HOUR;
+		int nowMinute = now.getMinute();
+		if (nowMinute >= 55 || nowMinute <= 5) {
+			result = String.valueOf(now.getHour());
+		}
+		return result;
 	}
 }
